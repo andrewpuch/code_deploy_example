0b49628786159208c3cf002842613e93ea669621
unknown <Andy@LAPTOP-PTDUJOHG>
2021-09-22 19:39:11 +0100

    Changing httpd to https

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yum -y install httpd > /var/log/installapache.out 2>&1
+yum -y install https > /var/log/installapache.out
--- scripts/startapache.sh
+++ scripts/startapache.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-service httpd start > /var/log/startapache.out 2>&1
+service https start > /var/log/startapache.out
